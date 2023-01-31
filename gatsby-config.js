5fc7599a29454fab17f392fa86c5b99f9b6a0d83
Stefan Gouldson <stefangouldson@gmail.com>
2023-01-31 02:19:40 +0000

    gatsby v5 tutorial finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,26 @@ module.exports = {
     title: `GatsbyV5`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+      __key: "blogs"
+    },
+    "gatsby-plugin-mdx"
+  ]
 };
